eef37670200997065cd5b0185156e7f2835b6a5e
unknown <shubhambahekar02@gmail.com>
2024-01-28 10:36:03 +0530

    updated the file

--- javascript_For_React/spread_operator.js
+++ javascript_For_React/spread_operator.js
@@ -4,21 +4,24 @@ const array = ["Shubham", "Rahul", "Pratik", "Naresh"];
 const Newarray = [array, "Sushil"];
 console.log(Newarray);
 
-//It wii add array values
-const Newarr = [...array, "Sushil"];
-console.log(Newarr);
+//to add new name/value in an array
+const newArrayValue = [...array, "Mahesh", "Suresh"];
+console.log("To Add NewArrayValue=>", newArrayValue);
 
-let obj = { Myname: "Shubham", date: "2012-02-31", age: 26 };
-console.log(obj);
+//spread operator should be at last in array destructuring
+const [FirstName, SecondName, ...otherNames] = array;
+console.log("TotalArr=>", otherNames);
 
-// It will add new object structure directly
-const obj2 = { obj, Myname: "Pratik" };
+let object = { Myname: "Shubham", date: "2012-02-31", age: 26 };
+
+// It will add new object structure directly and Myname is not updating
+const obj2 = { object, Myname: "Pratik" };
 console.log(obj2);
 
-// it will add new object values directly
-const obj3 = { ...obj, Myname: "Pratik" };
+// it will add new object values directly and Myname is  updated
+const obj3 = { ...object, Myname: "Pratik" };
 console.log(obj3);
 
-//spread operator should be at 1st place otherwise before that all keys not going to update
-const obj4 = { Myname: "Ganesh", ...obj, age: 20 };
+//spread operator should be at 1st place otherwise before that all key values not going to update
+const obj4 = { Myname: "Ganesh", ...object, age: 20 };
 console.log(obj4);
