ca1f11f643bf39073a4ea32424ba647bcd5729da
ShubhamBahekar <shubhambahekar02@gmail.com>
2024-02-15 21:51:21 +0530

    updated the code in sort function

--- javascript_For_React/sort.js
+++ javascript_For_React/sort.js
@@ -38,10 +38,26 @@ const object = [
   },
 ];
 
+//sort method modifies the original array
+const array = [2, 3, 5, 1, 8, 6, 3, 4, 7];
+// const newArray = array.sort((a, b) => a - b);
+// console.log(array);
+// console.log(newArray);
+
+//To avoid this we will use either slice() or spread operator
+//It will  copy values of original array and store in new array
+const newArray2 = [...array].sort((a, b) => a - b);
+console.log(array);
+console.log(newArray2);
+
+const newArray3 = array.slice().sort((a, b) => a - b);
+console.log(array);
+console.log(newArray3);
+
 // Ascending order [a-b]
-const sort_Ascending_Age = object.sort((a, b) => a.id - b.id);
+const sort_Ascending_Age = [...object].sort((a, b) => a.id - b.id);
 console.log(sort_Ascending_Age);
 
 //Descending order [b-a]
-const sort_Descending_Age = object.sort((a, b) => b.age - a.age);
+const sort_Descending_Age = object.slice().sort((a, b) => b.age - a.age);
 console.log(sort_Descending_Age);
