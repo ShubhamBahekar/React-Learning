9c3a55053ec87cc1b22e70a3592ec62adb7b71b3
ShubhamBahekar <shubhambahekar02@gmail.com>
2024-02-15 17:21:48 +0530

    updated filter file code

--- javascript_For_React/filter.js
+++ javascript_For_React/filter.js
@@ -1,9 +1,41 @@
 const object = [
-  { title: "A jindgi", author: "shubham", id: 1, age: 25 },
-  { title: "hm hai rahi pyar ke", author: "sushil", id: 2, age: 26 },
-  { title: "jo jita vhi sikandar", author: "pratik", id: 3, age: 27 },
-  { title: "Moment of the day", author: "naresh", id: 4, age: 28 },
-  { title: "tuta hua khwab hu mai", author: "pratik", id: 5, age: 29 },
+  {
+    title: "A jindgi",
+    author: "shubham",
+    id: 1,
+    age: 25,
+    favSports: ["football", "hockey"],
+  },
+  {
+    title: "hm hai rahi pyar ke",
+    author: "sushil",
+    id: 2,
+    age: 26,
+    isAvailable: false,
+    favSports: ["cricket", "hockey"],
+  },
+  {
+    title: "jo jita vhi sikandar",
+    author: "pratik",
+    id: 3,
+    age: 27,
+    isAvailable: true,
+    favSports: ["football", "cricket"],
+  },
+  {
+    title: "Moment of the day",
+    author: "naresh",
+    id: 4,
+    age: 28,
+    favSports: ["running", "hockey"],
+  },
+  {
+    title: "tuta hua khwab hu mai",
+    author: "pratik",
+    id: 5,
+    age: 29,
+    favSports: ["football", "hockey"],
+  },
 ];
 
 //1st approach
@@ -18,3 +50,7 @@ console.log(value2);
 
 const value3 = object.map((ele) => ele.age > 26 && ele.author === "pratik");
 console.log(value3);
+
+//In object/array  we  set a condition to filter values/objects which includes that values
+const val = object.filter((ele) => ele.favSports.includes("cricket"));
+console.log(val);
