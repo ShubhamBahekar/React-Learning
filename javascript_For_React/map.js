cc6fb45d03238f9cdcb7f107cd2bb9c99ec2b72a
ShubhamBahekar <shubhambahekar02@gmail.com>
2024-02-15 14:54:43 +0530

    first commit

---
+++ javascript_For_React/map.js
@@ -0,0 +1,33 @@
+const object = [
+  { title: "A jindgi", author: "shubham", id: 1, age: 25 },
+  { title: "hm hai rahi pyar ke", author: "sushil", id: 2, age: 26 },
+  { title: "jo jita vhi sikandar", author: "pratik", id: 3, age: 27 },
+  { title: "Moment of the day", author: "naresh", id: 4, age: 28 },
+  { title: "tuta hua khwab hu mai", author: "vaibhav", id: 5, age: 29 },
+];
+
+//It will iterate for every element and return an  elements with new array
+const array = [1, 2, 3, 4, 5];
+const newArray = array.map((element) => element * 2);
+console.log(newArray);
+
+const Authors = object.map((ele) => ele.author);
+console.log(Authors);
+
+//1st approach
+const newObject = object.map((ele) => {
+  return { author: ele.author, title: ele.title };
+});
+console.log(newObject);
+
+const getNewId = (ele) => {
+  return ele.id + 100;
+};
+
+//2nd approach,In this no need to use curly brace with return keyword
+const newObject2 = object.map((ele) => ({
+  author: ele.author,
+  title: ele.title,
+  newId: getNewId(ele),
+}));
+console.log(newObject2);
